5cf71cfb3a8a9d466b55e7067f2458296fe48772
ealmeida11 <eric.almeida365@gmail.com>
2026-02-27 15:53:12 -0300

    Monitor V2: Fri 02/27/2026 15:53:11.62

--- scraper_otimizado.py
+++ scraper_otimizado.py
@@ -277,6 +277,7 @@ class UnifiedNewsScraper:
         # Configurações otimizadas para melhor performance e estabilidade
         chrome_args = [
             # "--headless",
+            "--user-data-dir=/home/seluser/chrome-profile",
             "--disable-gpu",
             "--no-sandbox",
             "--disable-dev-shm-usage",
