0cfff20e53000937231cd6fb71fea8d097052727
ealmeida11 <eric.almeida365@gmail.com>
2026-02-27 15:52:52 -0300

    Monitor V2: Fri 02/27/2026 15:52:51.59

--- news_monitor_v2/coletores/folha.py
+++ news_monitor_v2/coletores/folha.py
@@ -215,6 +215,7 @@ def main():
 
     chrome_options = ChromeOptions()
     # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--user-data-dir=/home/seluser/chrome-profile")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
